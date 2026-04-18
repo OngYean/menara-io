42261daa0932060cc7178f1616155445268ef8d5
OngYean <79567278+OngYean@users.noreply.github.com>
2026-04-19 01:58:52 +0800

    feat: add tutorial scene and integrate navigation from main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,10 +2,15 @@ extends Control
 
 @onready var _singleplayer_button: Button = $SingleplayerButton
 @onready var _duo_button: Button = $DuoButton
+@onready var _tutorial_button: Button = $TutorialButton
 
 func _ready() -> void:
 	_singleplayer_button.pressed.connect(_on_singleplayer_pressed)
 	_duo_button.pressed.connect(_on_duo_pressed)
+	_tutorial_button.pressed.connect(_on_tutorial_pressed)
+
+func _on_tutorial_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
 func _on_singleplayer_pressed() -> void:
 	get_node("/root/Global").game_mode = "singleplayer"
